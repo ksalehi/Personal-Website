dfb0e7ef1988446323d2f4366e8e603233669219
Kia Salehi <ksalehi@wellesley.edu>
2016-07-24 16:16:07 -0700

    jquery working

--- entry.js
+++ entry.js
@@ -1,1 +1,20 @@
-const $ = require('jquery');
+
+$(document).ready( () => {
+  $('.about').on('click', e => {
+    console.log('hi');
+    hideCoverPhoto();
+  });
+  $('.resume').on('click', e => {
+    hideCoverPhoto();
+  });
+  $('.projects').on('click', e => {
+    hideCoverPhoto();
+  });
+  $('.contact').on('click', e => {
+    hideCoverPhoto();
+  });
+});
+
+function hideCoverPhoto() {
+  $('.cover-photo').addClass('hidden');
+}
